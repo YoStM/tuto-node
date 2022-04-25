bbc69951993c028512fa5c58e4aef8c489b7bac5
YoStM <yo.stm@outlook.com>
2022-04-25 20:32:22 +0200

    add middleware - body-parser

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const EXPRESS = require('express');
-const MORGAN = require('morgan');
-const FAVICON = require('serve-favicon');
-const {success} = require('./helper');
-let pokemons = require('./mock-pokemon');
+const EXPRESS = require("express");
+const MORGAN = require("morgan");
+const FAVICON = require("serve-favicon");
+const BODY_PARSER = require("body-parser");
+const { success, getUniqueId } = require("./helper");
+let pokemons = require("./mock-pokemon");
 
 const APP = EXPRESS();
 const PORT = 3000;
 
-APP.use(FAVICON(__dirname + '/favicon.ico')).use(MORGAN('dev'));
+APP.use(FAVICON(__dirname + "/favicon.ico"))
+  .use(MORGAN("dev"))
+  .use(BODY_PARSER.json());
 
-APP.get('/', (req,res) => res.send('Mon pokédex avec Express.js !'));
-APP.get('/api/pokemons', (req,res) => {
-    const MESSAGE = 'la liste des pokémons a bien été récupérée.';
-    res.json(success(MESSAGE,pokemons));
-})
-APP.get('/api/pokemons/:id', (req,res) => {
-    const ID = parseInt(req.params.id);
-    const POKEMON = pokemons.find(pokemon => pokemon.id === ID);
-    const MESSAGE = 'Un pokémon a bien été trouvé.';
-    res.json(success(MESSAGE, POKEMON));
+APP.get("/", (req, res) => res.send("Mon pokédex avec Express.js !"));
+APP.get("/api/pokemons", (req, res) => {
+  const MESSAGE = "la liste des pokémons a bien été récupérée.";
+  res.json(success(MESSAGE, pokemons));
+});
+APP.get("/api/pokemons/:id", (req, res) => {
+  const ID = parseInt(req.params.id);
+  const POKEMON = pokemons.find((pokemon) => pokemon.id === ID);
+  const MESSAGE = "Un pokémon a bien été trouvé.";
+  res.json(success(MESSAGE, POKEMON));
 });
 
-APP.listen(PORT, () => console.log(`Notre application Node est démarrée sur : http://localhost:${PORT}`));
+APP.post("/api/pokemons", (req, res) => {
+  const ID = getUniqueId(pokemons);
+  const POKEMON_CREATED = { ...req.body, ...{ id: ID, created: new Date() } };
+  pokemons.push(POKEMON_CREATED);
+  const MESSAGE = `Le pokémon ${POKEMON_CREATED.name} a bien été crée.`;
+  res.json(success(MESSAGE, POKEMON_CREATED));
+});
+
+APP.listen(PORT, () =>
+  console.log(
+    `Notre application Node est démarrée sur : http://localhost:${PORT}`
+  )
+);
--- helper.js
+++ helper.js
@@ -1,3 +1,11 @@
-exports.success = (message,data) => {
-    return { message,data };
-}
+exports.success = (message, data) => {
+  return { message, data };
+};
+
+exports.getUniqueId = (pokemons) => {
+  const POKEMONS_IDS = pokemons.map((pokemon) => pokemon.id);
+  const MAX_ID = POKEMONS_IDS.reduce((a, b) => Math.max(a, b));
+  const UNIQUE_ID = MAX_ID + 1;
+
+  return UNIQUE_ID;
+};
