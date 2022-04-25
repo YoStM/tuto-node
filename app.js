25f15db675d37dbc6f2a09a61a088c5d164a2f1e
YoStM <yo.stm@outlook.com>
2022-04-25 21:06:50 +0200

    add ORM - sequelize & db driver - mariadb

--- app.js
+++ app.js
@@ -32,6 +32,24 @@ APP.post("/api/pokemons", (req, res) => {
   res.json(success(MESSAGE, POKEMON_CREATED));
 });
 
+APP.put("/api/pokemons/:id", (req, res) => {
+  const ID = parseInt(req.params.id);
+  const POKEMON_UPDATED = { ...req.body, id: ID };
+  pokemons = pokemons.map((pokemon) => {
+    return pokemon.id === ID ? POKEMON_UPDATED : pokemon;
+  });
+  const MESSAGE = `Le pokémon ${POKEMON_UPDATED.name} a bien été modifié.`;
+  res.json(success(MESSAGE, POKEMON_UPDATED));
+});
+
+APP.delete("/api/pokemons/:id", (req, res) => {
+  const ID = parseInt(req.params.id);
+  const POKEMON_DELETED = pokemons.find((pokemon) => pokemon.id === ID);
+  pokemons.filter((pokemon) => pokemon.id !== ID);
+  const MESSAGE = `Le pokémon ${POKEMON_DELETED.name} a bien été supprimé.`;
+  res.json(success(MESSAGE, POKEMON_DELETED));
+});
+
 APP.listen(PORT, () =>
   console.log(
     `Notre application Node est démarrée sur : http://localhost:${PORT}`
